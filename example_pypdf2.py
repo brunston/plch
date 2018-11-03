c472f0db931501cfa940829d412b07f59bd0d3a8
brunston <brunston@protonmail.com>
2018-11-03 15:28:07 -0700

    pprint test and some more server stuff

---
+++ example_pypdf2.py
@@ -0,0 +1,17 @@
+import PyPDF2
+import pprint
+
+pprinter = pprint.PrettyPrinter().pprint
+
+pdf = open('ee16a-reader.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdf)
+
+outline = pdfReader.getOutlines()
+
+first_chapter = pdfReader.getDestinationPageNumber(outline[0])
+
+#pprinter(outline)
+
+#pprinter(first_chapter)
+
+pprinter(outline[1])
--- server/server.py
+++ server/server.py
@@ -3,9 +3,24 @@ Flask server for PLCH
 """
 
 from flask import Flask
+import pymongo
+import jsonify
 app = Flask(__name__)
 
+
 #https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+
+DB_NAME = "plchdb"
+COLLECTION_TEXTBOOKS = "textbook"
+COLLECTION_GRAPHS = "graph"
+COLLECTION_TEST = "test"
+
+try:
+    m_client = pymongo.MongoClient('mongodb://localhost:27017')
+    m_client.server_info()
+except pymongo.errors.ServerSelectionTimeoutError as e:
+    print("mongod local service not detected")
+    sys.exit()
 
 @app.route("/")
 def hello():
@@ -13,6 +28,14 @@ def hello():
     # return flask.render_template("danny_html")
 
 @app.route("/api/0.1/textbooks", methods=['GET'])
-def get_textbooks():
+def get_textbook_list():
     # get textbooks from mongodb
     #return jsonify(textbookthing)
+
+@app.route("/api/0.1/graphs", methods=['GET'])
+def get_graph_list():
+    # get a list of graphs from mongodb
+
+@app.route("/api/0.1/unstable/test", methods=['GET'])
+def get_unstable_test():
+    # get a list of things from the test database
