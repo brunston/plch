ba0d4e9913796e2393d9cc843fc83d0f4a80ef45
maryyliu <mary.y.liu@berkeley.edu>
2018-11-03 21:44:28 -0700

    outline attrib for pdf_extractor

--- server/pdf_extractor.py
+++ server/pdf_extractor.py
@@ -4,43 +4,43 @@ import sys
 class pdfExtractor:
     def __init__(self, pdf_file):
         self.pdfReader = PyPDF2.PdfFileReader(open(pdf_file, 'rb'))
+        self.outline = self.pdfReader.getOutlines()
 
     def get_headers(self):
-        outline = self.pdfReader.getOutlines()
         result = list()
 
-        for i in range(len(outline)):
-            item = outline[i]
+        for i in range(len(self.outline)):
+            item = self.outline[i]
             ## checks for subheaders
             if isinstance(item, list):
                 ## add subheader sections to result list as nested list
-                helper(outline, i, result)
+                self.helper(i, result)
             else:
                 ## add list containing header title, page number, text respectively to result list
                 try:
-                    toAdd = [item['/Title'], pdfReader.getDestinationPageNumber(item)]
+                    toAdd = [item['/Title'], self.pdfReader.getDestinationPageNumber(item)]
                     result.append(toAdd)
                 except:
                     continue
 
-        generateText(result)
+        self.generateText(result)
         return result
 
 
-    def helper(self, outline, index, res):
+    def helper(self, index, res):
 
         new_list = list()
         res.append(new_list)
 
-        subsections = outline[index]
+        subsections = self.outline[index]
 
         for i in range(len(subsections)):
             item = subsections[i]
             if isinstance(item, list):
-                helper(subsections, i, new_list)
+                self.helper(subsections, i, new_list)
             else:
                 try:
-                    toAdd = [item['/Title'], pdfReader.getDestinationPageNumber(item)]
+                    toAdd = [item['/Title'], self.pdfReader.getDestinationPageNumber(item)]
                     new_list.append(toAdd)
                 except:
                     continue
@@ -50,13 +50,17 @@ class pdfExtractor:
         for i in range(len(result)):
             page_start = result[i][1]
             text = ''
-            page_end = pdfReader.getNumPages()
+            page_end = self.pdfReader.getNumPages()
 
             if i < len(result) - 1:
                 page_end = result[i + 1][1]
 
             while page_start != page_end - 1:
-                text += pdfReader.getPage(page_start).extractText()
+                text += self.pdfReader.getPage(page_start).extractText()
                 page_start += 1
 
             result[i].append(text)
+
+test = pdfExtractor('pacs10-small.pdf')
+res = test.get_headers()
+print(res)
