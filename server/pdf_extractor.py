96e620221f8148ad82af32900ae9ef6fece27307
brunston poon <brupoon@outlook.com>
2018-11-04 01:51:48 -0700

    Feature/topic analysis (#3)
    * topic analysis
    
    * working on topic analysis
    
    * outline attrib for pdf_extractor
    
    * remove merp
    
    * some basic script exploration
    
    * trying different corpi

--- server/pdf_extractor.py
+++ server/pdf_extractor.py
@@ -1,29 +1,28 @@
 import PyPDF2
-import sys
 
-class pdfExtractor:
+class PdfExtractor:
     def __init__(self, pdf_file):
         self.pdfReader = PyPDF2.PdfFileReader(open(pdf_file, 'rb'))
+        self.outline = self.pdfReader.getOutlines()
 
     def get_headers(self):
-        outline = self.pdfReader.getOutlines()
         result = list()
 
-        for i in range(len(outline)):
-            item = outline[i]
+        for i in range(len(self.outline)):
+            item = self.outline[i]
             ## checks for subheaders
             if isinstance(item, list):
                 ## add subheader sections to result list as nested list
-                helper(outline, i, result)
+                self.helper(self.outline, i, result)
             else:
                 ## add list containing header title, page number, text respectively to result list
                 try:
-                    toAdd = [item['/Title'], pdfReader.getDestinationPageNumber(item)]
+                    toAdd = [item['/Title'], self.pdfReader.getDestinationPageNumber(item)]
                     result.append(toAdd)
                 except:
                     continue
 
-        generateText(result)
+        self.generate_text(result)
         return result
 
 
@@ -37,26 +36,26 @@ class pdfExtractor:
         for i in range(len(subsections)):
             item = subsections[i]
             if isinstance(item, list):
-                helper(subsections, i, new_list)
+                self.helper(subsections, i, new_list)
             else:
                 try:
-                    toAdd = [item['/Title'], pdfReader.getDestinationPageNumber(item)]
+                    toAdd = [item['/Title'], self.pdfReader.getDestinationPageNumber(item)]
                     new_list.append(toAdd)
                 except:
                     continue
 
-    def generateText(self, result):
+    def generate_text(self, result):
         #print len(result)
         for i in range(len(result)):
             page_start = result[i][1]
             text = ''
-            page_end = pdfReader.getNumPages()
+            page_end = self.pdfReader.getNumPages()
 
             if i < len(result) - 1:
                 page_end = result[i + 1][1]
 
             while page_start != page_end - 1:
-                text += pdfReader.getPage(page_start).extractText()
+                text += self.pdfReader.getPage(page_start).extractText()
                 page_start += 1
 
             result[i].append(text)
---
+++ server/topic_analysis.py
@@ -0,0 +1,32 @@
+import pdf_extractor
+from gensim import corpora, models, similarities
+import ast
+
+class TopicAnalyzer:
+    def __init__(self, pdf_file):
+        self.extractor = pdf_extractor.PdfExtractor(pdf_file)
+
+if __name__=="__main__":
+    import pprint
+    pprinter = pprint.PrettyPrinter().pprint
+    analyzer = TopicAnalyzer("pacs10-small.pdf")
+    topic_list = analyzer.extractor.get_headers()
+    #pprinter(topic_list[1])
+    topic_list = []
+    with open('cleanest_pacs.txt') as f:
+        topic_list = ast.literal_eval(f.read())
+    stoplist = set('for a of the and to in have was is or it has are on each fine one were me my her she he his their they them these (p.'.split())
+    texts = [[word for word in str(document).lower().split() if word not in stoplist] for document in topic_list]
+    #pprinter(texts)
+    dictionary = corpora.Dictionary(texts)
+    #print(dictionary.token2id)
+    #dictionary.save('pacs_corpora.dict')
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    #corpora.MmCorpus.serialize('pacs_corpus.mm', corpus) # save to disk
+    tfidf = models.TfidfModel(corpus) # initialize a model
+    corpus_tfidf = tfidf[corpus]
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2)
+    corpus_lsi = lsi[corpus_tfidf]
+    pprinter(lsi.print_topics(2))
+    for doc in corpus_lsi:
+        print(doc)
