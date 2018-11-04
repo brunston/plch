685bbc137b447c51f89c4eb99d3b3701a1f66bb2
Danny Chu <dannychu@berkeley.edu>
2018-11-04 15:13:14 -0800

    final draft for version 0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./components/BubbleBoxes.css";
 
 import { render } from "react-dom";
 import Modal from "react-modal";
@@ -15,17 +14,24 @@ class App extends Component {
     super(props);
     this.state = {
       isPaneOpen: false,
-      currTopic: "Linear Logistics"
+      currTopic: "Start",
+      currOrder: 0,
+      data: [],
+      docIds: []
     };
+    // this.state.data = this.createFakeTopicData();
+    this.state.data = [];
   }
 
-  componentDidMount() {
-    Modal.setAppElement(this.el);
+  myCallback(dataFromChild) {
+    this.setState({ currTopic: dataFromChild.topic });
+    this.setState({ currOrder: dataFromChild.order });
   }
+
 
   createFakeTopicData() {
-    const d = ["Linear Regression", "Linear Growth", "Quadratic Regression", "ReLu"];
-    const o = [-1, -1, 1, 1];
+    const d = ["Linear Regression", "Linear Growth", "Quadratic Regression", "ReLu", "Machine Learning"];
+    const o = [-1, -1, 1, 2, 3];
     const data = [];
     for (let x = 0; x <= d.length; x++) {
       let topic = d[x];
@@ -52,17 +58,42 @@ class App extends Component {
   //   return data;
   // }
 
+  componentDidMount() {
+    Modal.setAppElement(this.el);
+    fetch('http://b15da023.ngrok.io/api/v0.1/corpus', {mode: 'cors'})
+    .then(results => {
+      console.log(results);
+      return results.json();
+    }).then(data => {
+        this.state.docIds = data;
+        let doc = this.state.docIds;
+        for (var i = 0; i < 10; i++)
+        {
+            // Do something
+            let id = doc[i];
+            fetch('http://b15da023.ngrok.io/api/v0.1/corpus/text/' + id + '/heading', {mode: 'cors'})
+            .then(results2 => {
+              return results2.json();
+            }).then(data2 => {
+                console.log(data2);
+                let topic = data2[0];
+                let order = id;
+                this.state.data.push({ topic, order });
+            })        
+        }        
+    }) 
+
+    
+  }
+
   render() {
-    let currDataBoxes = this.createFakeTopicData().filter((boxes, i) => {
-      return boxes.order == 0;
-    });
     return (
       <div className="App">
         <div ref={ref => (this.el = ref)}>
-          <button className="btn btn-4 btn-4c" key={currDataBoxes.topic} onClick={() => this.setState({ isPaneOpen: true })}>
+          <button className="btn btn-4 btn-4c" onClick={() => this.setState({ isPaneOpen: true })}>
             {this.state.currTopic}
           </button>
-          <BubbleChart dataBoxes={this.createFakeTopicData()}/>
+          <BubbleChart dataBoxes={this.state.data} callbackFromParent={this.myCallback.bind(this)}/>
           <SlidingPane
               className='some-custom-class'
               overlayClassName='some-custom-overlay-class'
@@ -74,6 +105,7 @@ class App extends Component {
               } }>
               <br />
               <HTMLViewer />
+              <img src={require('./files/pdff.png')} style={{width:"1000px"}}/>
           </SlidingPane>
         </div>
       </div>
