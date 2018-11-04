9ee5de608b26f0c93eca9347cda31b21628249ea
brunston <brunston@protonmail.com>
2018-11-03 22:44:06 -0700

    some basic script exploration

--- server/topic_analysis.py
+++ server/topic_analysis.py
@@ -1,4 +1,5 @@
 import pdf_extractor
+from gensim import corpora, models, similarities
 import pandas
 
 
@@ -11,4 +12,17 @@ if __name__=="__main__":
     pprinter = pprint.PrettyPrinter().pprint
     analyzer = TopicAnalyzer("pacs10-small.pdf")
     topic_list = analyzer.extractor.get_headers()
-    pprinter(topic_list)
+    #pprinter(topic_list)
+    stoplist = set('for a of the and to in'.split())
+    texts = [[word for word in str(document).lower().split() if word not in stoplist] for document in topic_list]
+    #pprinter(texts)
+    dictionary = corpora.Dictionary(texts)
+    #print(dictionary.token2id)
+    #dictionary.save('pacs_corpora.dict')
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    #corpora.MmCorpus.serialize('pacs_corpus.mm', corpus) # save to disk
+    tfidf = models.TfidfModel(corpus) # initialize a model
+    corpus_tfidf = tfidf[corpus]
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=300)
+    corpus_lsi = lsi[corpus_tfidf]
+    pprinter(lsi.print_topics(300))
